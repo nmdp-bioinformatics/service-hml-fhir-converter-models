7c784dd95a8811c99333fc8e4a9f8b09bfb8cf21
Andrew S. Brown, Ph.D <abrown3@nmdp.org>
2017-08-09 10:19:38 -0500

    Updates

--- src/main/java/org/nmdp/hmlfhirconvertermodels/attributes/FhirPrimaryResource.java
+++ src/main/java/org/nmdp/hmlfhirconvertermodels/attributes/FhirPrimaryResource.java
@@ -29,7 +29,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 @Target(value = ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface FhirPrimaryResource {
